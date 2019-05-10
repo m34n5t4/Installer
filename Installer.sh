e8299053601945991fd06f0d041470d0699dd631
m34n5t4 <50254973+m34n5t4@users.noreply.github.com>
2019-05-10 17:34:19 +0700

    Create Installer.sh

---
+++ Installer.sh
@@ -0,0 +1,182 @@
+#!/data/data/com.termux/files/usr/bin/bash
+# Auto Install V.1
+# create by m34n5t4
+# mampu berkarya bkn cma bergaya
+clear
+
+echo "\033[38;1m ###########################"|lolcat
+echo "\033[31;1m ★Tools Installer★"
+echo "\033[36;1m Author By :M34N5T4"
+echo "\033[32;1m Thanks for:Cyber Net Muslim"
+
+echo "\033[38;1m ###########################"|lolcat
+
+date|lolcat
+
+toilet -f slant --gay "M34N5T4"
+
+echo  "#1==> GcoSpam "
+sleep 1
+echo  "#2==> LiteSpam "
+sleep 1
+echo  "#3==> Tool Mr.Rv1.1 "
+sleep 1
+echo  "#4==> NetHunter "
+sleep 1
+echo  "#5==> Metasploit "
+sleep 1
+echo  "#6==> Tool B4J1N94N "
+sleep 1
+echo  "#7==> Tool TU4NB4DUT "
+sleep 1
+echo  "#8==> Hack FB "
+sleep 1
+echo  "#9==> FBH "
+sleep 1
+echo  "#10==> LazhGo "
+
+sleep 1
+echo  "Pilih Nomor Berapa "
+read -p "[root@m34n5t4]# " act;
+
+if [ $act = 1 ] || [ $act = 1 ]
+then
+clear
+echo -e $green" installing GcoSpam "
+sleep 1
+git clone https://github.com/Amriez/gcospam
+cd gcospam
+sh install.sh
+sh gco.sh
+sleep 1
+echo -e $red" DONE "
+fi
+
+if [ $act = 2 ] || [ $act = 2 ]
+then
+clear
+echo -e $green" installing LITESPAM "
+sleep 1
+pkg install git php python2 cowsay figlet toilet ruby
+gem install lolcat
+pip2 install toilet
+pip2 install lolcat
+pkg install php
+git clone https://github.com/GUNAWAN18ID/B3604-spam-v1.git
+cd B3604-spam-v1
+bash B3604-spam.sh
+sleep 1
+echo -e $red" DONE "
+fi
+
+if [ $act = 3 ] || [ $act = 3 ]
+then
+clear
+echo -e $green" installing tool Mr.Rv1.1 "
+sleep 1
+pkg install gem
+pkg install figlet
+gem install lolcat
+git clone https://github.com/Mr-R225/Mr.Rv1.1
+cd Mr.Rv1.1
+sh Mr.Rv1.1.sh
+sleep 1
+echo -e $red" DONE "
+fi
+
+if [ $act = 4 ] || [ $act = 4 ]
+then
+clear
+echo -e $green" installing NetHunter "
+sleep 1
+git clone https://github.com/Hax4us/Nethunter-In-Termux.git
+cd Nethunter
+Is
+chmod +x kalinethunter
+./kalinethunter
+./startkali.sh
+sleep 1
+echo -e $red" DONE "
+fi
+
+if [ $act = 5 ] || [ $act = 5 ]
+then
+clear
+echo -e $green" installing  Metasploit "
+sleep 1
+pkg install curl
+curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+chmod +x metasploit.sh
+./metasploit.sh
+sleep 1
+echo -e $red" DONE "
+fi
+
+if [ $act = 6 ] || [ $act = 6 ]
+then
+clear
+echo -e $green" installing B4J1N94Nv5 "
+sleep 1
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5 cd B4J1N64Nv5
+sh B4J1N64N.sh
+sleep 1
+echo -e $red" DONE "
+fi
+
+if [ $act = 7 ] || [ $act = 7 ]
+then
+clear
+echo -e $green" installing TU4NB4DUT "
+sleep 1
+pkg install python python2 vim figlet curl
+pkg install php
+pip2 install lolcat
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+cd TOOLSINSTALLERv4
+ls
+chmod 777 TUANB4DUT..sh
+./TUANB4DUT..sh
+sleep 1
+echo -e $red" DONE "
+fi
+
+if [ $act = 8 ] || [ $act = 8 ]
+then
+clear
+echo -e $green" installing HACK FB "
+sleep 1
+pkg install nano python2 cowsay git
+pip2 install lolcat
+git clone https://github.com/DarkCurut08/DarkCurut08
+cd DarkCurut08
+sh Moreno77.sh
+sleep 1
+echo -e $red" DONE "
+fi
+
+if [ $act = 9 ] || [ $act = 9 ]
+then
+clear
+echo -e $green" installing FBH "
+sleep 1
+git clone https://github.com/muhammadfathul/FBH
+cd FBH
+chmod 777 *
+bash setup
+./FBH
+sleep 1
+echo -e $red" DONE "
+fi
+
+if [ $act = 10 ] || [ $act = 10 ]
+then
+clear
+echo -e $green" installing LazyGo "
+sleep 1
+git clone https://github.com/kadal15/LazyGo
+cd LazyGo
+sh install.sh
+sh lazygo.sh
+sleep 1
+echo -e $red" DONE "
+fi
